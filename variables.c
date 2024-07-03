0563ed7215b7adfe246c09885a0b193d63c5ca12
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-07-03 08:39:28 +0600

    start

---
+++ variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    float b = 5.55; // 5.55 is a real value so we can not use int here
+    char c = 'U';
+    printf("The value of a is %d \n", a); // \n for new line
+    printf("The value of b is %f \n", b);
+    printf("The value of b is %c ", c);
+    return 0;
+}
